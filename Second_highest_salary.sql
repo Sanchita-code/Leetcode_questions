448a7683dc7efa27e6ec8c83cd87746cd386697b
Sanchita-code <sanchitasrkr@gmail.com>
2025-08-17 11:53:44 +0530

    Update Second_highest_salary.sql
    updated query

--- Second_highest_salary.sql
+++ Second_highest_salary.sql
@@ -1,7 +1,9 @@
-With gather_data as (select id from Employee order by salary desc limit 1)
+With gather_data as (select id, salary from Employee order by salary desc limit 1)
 select case 
-        WHEN (select count(*) from Employee e1) > 1 Then max(salary) 
+        WHEN (select count(*) from Employee e1) > 2 Then max(e.salary) 
+        WHEN (select count(*) from Employee e1) = 2 Then e.salary
         ELSE null
         END AS SecondHighestSalary
 from Employee e, gather_data gd
-where e.id <> gd.id;
+where e.id <> gd.id
+  and e.salary <> gd.salary;
